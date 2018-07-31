2510dbff1f30c7ed2fd028c49f266067253b79fa
Mark Donahue <mark-donahue@idexx.com>
2018-07-31 10:23:43 -0400

    ADMIN add files from generated project template

---
+++ src/main/java/net/katrinka/sb2ongcp/Sb2OnGcpApplication.java
@@ -0,0 +1,12 @@
+package net.katrinka.sb2ongcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2OnGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2OnGcpApplication.class, args);
+	}
+}
---
+++ src/test/java/net/katrinka/sb2ongcp/Sb2OnGcpApplicationTests.java
@@ -0,0 +1,16 @@
+package net.katrinka.sb2ongcp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sb2OnGcpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
